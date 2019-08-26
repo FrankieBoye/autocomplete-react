9b0162bceb2c2653c60e1245b1e015cecac3597c
FrankieBoye <frankie4@btinternet.com>
2019-08-26 21:58:49 +0100

    removed comments from app.js file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import AutoCompleteText from './AutoCompleteText';
 class App extends Component {
   render () {
     return (
-//app.js is main top level component
-//import components module
-//add module as html tag
+
     <div className="App">
 
     <AutoCompleteText />
